4d27a32ff00b006369ff634eadf5cf0983a17f95
Rodrigo Luger <rodluger@gmail.com>
2018-10-30 09:37:54 -0400

    simpler

--- fluence/example.py
+++ fluence/example.py
@@ -6,36 +6,13 @@ import fluence
 exp = 0.45
 r = 0.1
 u = np.array([0.4, 0.26])
-
-
 b = np.linspace(-1.5, 1.5, 100)
+
 f = fluence.flux(b, r, u)
-
 fl = fluence.exact_fluence(b, r, u, exp)
-rf = fluence.riemann_fluence(b, r, u, exp, 50)
-trf = fluence.trapezoid_fluence(b, r, u, exp, 50)
-sf = fluence.simpson_fluence(b, r, u, exp, 8)
-tf2 = fluence.taylor_fluence(b, r, u, exp, 2)
-lrf = fluence.left_riemann_fluence(b, r, u, exp, 50)
 
-fig, ax = pl.subplots(2, figsize=(8, 8))
-ax[0].plot(b, f)
-ax[0].plot(b, tf2)
-ax[0].plot(b, rf)
-ax[0].plot(b, trf)
-ax[0].plot(b, sf)
-ax[0].plot(b, lrf)
-ax[0].plot(b, fl, 'k', lw=1)
-ax[0].set_ylim(0.9875 , 1.0005)
-
-ax[1].plot(b, np.abs(f - fl))
-ax[1].plot(b, np.abs(tf2 - fl))
-ax[1].plot(b, np.abs(rf - fl))
-ax[1].plot(b, np.abs(trf - fl))
-ax[1].plot(b, np.abs(sf - fl))
-ax[1].plot(b, np.abs(lrf - fl))
-ax[1].set_yscale("log")
-
-# Plot the bounds
-
+fig, ax = pl.subplots(1, figsize=(8, 5))
+ax.plot(b, f)
+ax.plot(b, fl, 'k', lw=1)
+ax.set_ylim(0.9875 , 1.0005)
 pl.show()
